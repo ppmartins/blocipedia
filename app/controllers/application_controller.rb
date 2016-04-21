34a797b4d1442871ee06058634b265384aa1267b
Pedro <ppereiramartins@gmail.com>
2016-04-21 15:25:53 +0100

    Fixing routes work in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    wikis_path(resource)
+    session[:previous_url] || wikis_path
   end
 end
